1faf8d57b7c221419a7b108cac51810230fb56d7
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-26 14:21:26 -0800

    Got a functioning test

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,19 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { createStatsString } from '../utils.js';
 
 const test = QUnit.test;
+
+
+test('Testing my string', (expect) => {
+    const string = `You have changed the head duck times, the body Fancy times, and the pants white times. And nobody can forget your character's classic catchphrases:`;
+
+    const actual = createStatsString('duck', 'Fancy', 'white');
+
+    expect.equal(actual, string);
+});
+
+
 
 test('time to test a function', (expect) => {
     //Arrange
