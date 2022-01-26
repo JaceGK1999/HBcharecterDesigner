7d3e52558b0f868a8d3f74d3d0c23c42a52db6d9
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-26 13:34:59 -0800

    Got bottom  dropdown working

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ catchphraseButton.addEventListener('click', () => {
 
 function displayStats() {
     const statsString = createStatsString(headCount, middleCount, bottomCount);
-    //return `You have changed the head ${headCount} times, the body ${middleCount} times, and the pants ${bottomCount} times. And nobody can forget your character's classic catchphrases:`;
     reportEl.textContent = statsString;
 }
 
