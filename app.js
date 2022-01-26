38b00833ea71d3e6ce54b9eccd737a5fb0407dc2
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-26 12:08:56 -0800

    added utils and changed values

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+import { createStatsString } from './utils';
+
 const headDropdown = document.getElementById('head-dropdown');
 const middleDropdown = document.getElementById('middle-dropdown');
 const bottomDropdown = document.getElementById('bottom-dropdown');
 const headEl = document.getElementById('top');
+const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
-const chatchphrasesEl = document.getElementById('chatch-phrases');
+const chatchphrasesEl = document.getElementById('catch-phrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
+const catchphraseButton = document.getElementById('catchphrase-button');
 
-const headCount = 0;
-const middleCount = 0;
-const bottomCount = 0;
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
 
-const catchphrases = 0;
+const catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
     const value = headDropdown.value;
@@ -39,16 +43,18 @@ bottomDropdown.addEventListener('change', () => {
     displayStats();
 });
 
-catchphraseButton.addEventListener = () => {
+catchphraseButton.addEventListener = ('click', () => {
     const newCatchphrase = catchphraseInput.value;
     catchphrases.push(newCatchphrase);
-
-    catchphraseInput.value = '';
+    
     displayCatchphrases();
-};
+    catchphraseInput.value = '';
+});
 
 function displayStats() {
-    return `You have changed the head ${headCount} times, the body ${middleCount} times, and the pants ${bottomCount} times. And nobody can forget your character's classic catchphrases:`;
+    const statsString = createStatsString(headCount, middleCount, bottomCount);
+    //return `You have changed the head ${headCount} times, the body ${middleCount} times, and the pants ${bottomCount} times. And nobody can forget your character's classic catchphrases:`;
+    reportEl.textContent = statsString;
 }
 
 function displayCatchphrases() {
@@ -61,3 +67,4 @@ function displayCatchphrases() {
         chatchphrasesEl.append(p);
     }
 }
+
